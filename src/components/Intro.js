b725a79fedada121f0a70173e57295475e685ad1
Daniel Peter Kuc <danielpiotrkuc@gmail.com>
2021-04-04 21:50:54 +0100

    added background

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,16 +1,19 @@
 import React from 'react';
+import Background from './Background'
 
 const Intro = () => {
   return (
-    <div className="container-flex d-flex vh-100 background justify-content-center align-items-center">
+    <Background>
+      <div className="container-flex d-flex vh-100 background justify-content-center align-items-center">
       <div className="row">
-        <div className=" test">
-          <p className="d-inline vertical-text align-top">Daniel</p>
-          <p className="d-inline vertical-text align-top">Peter</p>
-          <p className="d-inline vertical-text align-top">Kuc</p>
+        <div className="d-flex">
+          <p className="vertical-text align-top">Daniel</p>
+          <p className="vertical-text align-top">Peter</p>
+          <p className="vertical-text align-top">Kuc</p>
         </div>
       </div>
     </div>
+      </Background>
   )
 }
 
