9e301023d1bbc6f8f8a65c6bd2aa22e9bc542c80
Daniel Peter Kuc <danielpiotrkuc@gmail.com>
2021-04-07 21:23:22 +0100

    added about component styling

--- src/components/About.js
+++ src/components/About.js
@@ -4,13 +4,28 @@ import styled from 'styled-components';
 const About = () => {
   return (
     <StyledBackground>
-      Hello from About
+      <div className="container">
+        <div className="row d-flex justify-content-around">
+          <div className="col-12 col-12-md my-5 box">
+            <p>About Me</p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam temporibus distinctio omnis laudantium labore delectus dicta, quos eos officiis sed quas tenetur quo pariatur earum repellat commodi tempore. Doloribus, quis.
+          </div>
+          <div className="col-12 col-md-6 my-5 box">
+            <p>Inspiration</p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi beatae architecto, quibusdam molestias nobis, repellat cupiditate nisi illo natus cumque dignissimos magni quae! Perferendis recusandae nesciunt dolorem ea, suscipit eum!
+          </div>
+          <div className="col-12 col-md-6 my-5 box">
+            <p>Philosophy</p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima eveniet accusamus sapiente ratione veniam amet, libero eum consequatur nulla minus impedit sint reprehenderit quis officiis voluptatibus nemo provident sit explicabo?
+          </div>
+        </div>
+      </div>
     </StyledBackground>
   )
 }
 
 const StyledBackground = styled.div`
-  height: 100vh;
+  min-height: 100vh;
   background-color: #082447;
   color:white ;
 `
