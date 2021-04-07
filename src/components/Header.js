a34fa3b188922d7c485bb2595ae5826fad7a9700
Daniel Kuc <danielpiotrkuc@gmail.com>
2021-04-07 15:22:42 +0100

    changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <div className="text-secondary">
-      <p className="fadeInDown">Front End Developer</p>
+      <p className="fadeInUp">Front End Developer</p>
     </div>
   )
 }
