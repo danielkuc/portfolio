503dbbe094ba4ba7cf0ae832851ddd7d3575db91
Daniel Peter Kuc <danielpiotrkuc@gmail.com>
2021-04-08 23:08:08 +0100

    added AboutMe component animations

--- src/components/About.js
+++ src/components/About.js
@@ -10,11 +10,11 @@ const About = () => {
             <p>About Me</p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam temporibus distinctio omnis laudantium labore delectus dicta, quos eos officiis sed quas tenetur quo pariatur earum repellat commodi tempore. Doloribus, quis.
           </div>
-          <div className="col-12 col-md-6 my-5">
+          <div className="col-12 col-md-6 my-5 fadeInLeft">
             <p>Inspiration</p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi beatae architecto, quibusdam molestias nobis, repellat cupiditate nisi illo natus cumque dignissimos magni quae! Perferendis recusandae nesciunt dolorem ea, suscipit eum!
           </div>
-          <div className="col-12 col-md-6 my-5">
+          <div className="col-12 col-md-6 my-5 fadeInRight">
             <p>Philosophy</p>
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima eveniet accusamus sapiente ratione veniam amet, libero eum consequatur nulla minus impedit sint reprehenderit quis officiis voluptatibus nemo provident sit explicabo?
           </div>
