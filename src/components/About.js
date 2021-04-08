66b06c1aa1c40dae3c68c006c629811f4f49c6a7
Daniel Kuc <danielpiotrkuc@gmail.com>
2021-04-08 15:20:48 +0100

    styling changed

--- src/components/About.js
+++ src/components/About.js
@@ -5,16 +5,16 @@ const About = () => {
   return (
     <StyledBackground>
       <div className="container">
-        <div className="row d-flex justify-content-around">
-          <div className="col-12 col-12-md my-5 box">
+        <div className="row">
+          <div className="col-12 col-12-md my-5 fadeIn">
             <p>About Me</p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam temporibus distinctio omnis laudantium labore delectus dicta, quos eos officiis sed quas tenetur quo pariatur earum repellat commodi tempore. Doloribus, quis.
           </div>
-          <div className="col-12 col-md-6 my-5 box">
+          <div className="col-12 col-md-6 my-5">
             <p>Inspiration</p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi beatae architecto, quibusdam molestias nobis, repellat cupiditate nisi illo natus cumque dignissimos magni quae! Perferendis recusandae nesciunt dolorem ea, suscipit eum!
           </div>
-          <div className="col-12 col-md-6 my-5 box">
+          <div className="col-12 col-md-6 my-5">
             <p>Philosophy</p>
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima eveniet accusamus sapiente ratione veniam amet, libero eum consequatur nulla minus impedit sint reprehenderit quis officiis voluptatibus nemo provident sit explicabo?
           </div>
