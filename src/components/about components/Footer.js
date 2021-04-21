ddbafb416de9ea080f17c7eb9c6867049657523c
Daniel Kuc <danielpiotrkuc@gmail.com>
2021-04-21 16:46:02 +0100

    added project component styling

--- src/components/about components/Footer.js
+++ src/components/about components/Footer.js
@@ -5,10 +5,10 @@ const Footer = () => {
   const [ref,visible] = useOnScreen({threshold: 0.5});
 
   return (
-    <div className={`call-to-action col-12 ${visible ? "fadeIn" : null} `}>
+    <div ref={ref} className={`call-to-action col-12 ${visible ? "fadeIn" : null} `}>
       <p className="h3">Lets be creative together!</p>
       <p>...or have a coffee</p>
-      <a href="#">Get in touch!</a>
+      <a href="mailto:danpkuc@gmail.com">Get in touch!</a>
   </div>
   )
 }
