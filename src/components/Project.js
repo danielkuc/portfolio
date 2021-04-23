be3a5e43dbddad64b9e284687cffeebbbb65206f
Daniel Kuc <danielpiotrkuc@gmail.com>
2021-04-23 15:45:51 +0100

    changed typography

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Project = ({params}) => {
-  const {title, description,logos, github, projectImg} = params
+  const {title, description,logos, github, projectImg, alt} = params
   return (
       <div className="card col-12 col-md-6 col-lg-4 mt-3">
         <img className="card-img-top" src={projectImg} alt={projectImg}/>
@@ -9,7 +9,7 @@ const Project = ({params}) => {
           <p className="card-title">{title}</p>
           <p className="card-text">{description}</p>
           <div className="d-flex img-list">
-            {logos.map((img, index) => <img src={img} alt="" className="card-logo" key={index}/>)}
+            {logos.map((img, index) => <img src={img} alt={alt[index]} className="card-logo" key={index}/>)}
           </div>
           <a className="card-link" href={github} target={"_blank"} rel="noreferrer">Source code</a>
           <button className="btn btn-info d-block my-3">Live project</button>
