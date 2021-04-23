498973f9963493bcc8e17084800d11b055206bb6
Daniel Kuc <danielpiotrkuc@gmail.com>
2021-04-23 07:43:19 +0100

    small changes

--- src/components/about components/About.js
+++ src/components/about components/About.js
@@ -6,7 +6,7 @@ const About = () => {
 
   return (
     <section ref={ref} className={`col-12 col-md-12 mb-5 ${visible ? "fadeIn" : "fadeOut"}`}>
-      <span className="d-block">Good question, quick introductions are in order!
+      <span className="d-block mb-2">Good question, quick introductions are in order!
       My name is Dan, nice to meet you!</span>
       I am a front end, aspiring to full stack, developer and I hail from the sunny planet Earth,
       lovely place by the way, you should come and visit. Internet, software, applications and websites are my passion and were my hobby for a long time. After a successful career in health care I have decided to take a leap and commit to my hobby full time.
