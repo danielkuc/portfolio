16dc2d92e97820b62af371bce2f0a31955fba52b
Daniel Kuc <danielpiotrkuc@gmail.com>
2021-05-11 21:23:00 +0100

    changed styling

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -21,7 +21,7 @@ const ProjectList = () => {
   return (
     <div className="project p-3 mb-5">
       <div className="container">
-        <p className="h4 text-center py-2">Some of the projects I worked on.</p>
+        <p className="h3 text-center py-2">Some of the projects I worked on.</p>
         <p>These are the projects I've spent most time working on and keep coming back to. Some of them are passion projects, some are learning projects. Most are made for fun!</p>
         <div className="row">
           {projects.map(project => {
