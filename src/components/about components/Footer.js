1bec04557dfa922b4e8c0078df52b6eb78841f95
Daniel Kuc <danielpiotrkuc@gmail.com>
2021-05-11 19:58:41 +0100

    changes

--- src/components/about components/Footer.js
+++ src/components/about components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
 
   return (
     <section ref={ref} className={`call-to-action col-12 py-2 my-2 d-flex flex-wrap flex-column align-items-center ${visible ? "fadeIn" : null} `}>
-      <p className="h3">Lets be creative together!</p>
+      <p className="h3">Let's get creative together!</p>
       <p>...or have a coffee</p>
       <a className="mb-5" href="mailto:danpkuc@gmail.com">Get in touch!</a>
     </section>
