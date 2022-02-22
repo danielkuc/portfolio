6e1c9d888d455465fbe9825a76a12f4f83942482
Daniel Kuc <danpkuc@gmail.com>
2022-02-22 21:17:06 +0000

    updated projects and skills section

--- src/components/about components/Skills.js
+++ src/components/about components/Skills.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import useOnScreen from '../hooks';
 
-import css from '../../images/css3.svg';
 import csharp from '../../images/c.svg';
-import html from '../../images/html5.svg';
+import core from '../../images/core.svg';
 import js from '../../images/javascript.svg';
-import php from '../../images/php.svg';
-import laravel from '../../images/laravel.svg';
 import react from '../../images/react.svg';
-import redux from '../../images/redux.svg';
 
 const Skills = () => {
   const [ref, visible] = useOnScreen({threshold: 0.2});
@@ -18,17 +14,10 @@ const Skills = () => {
     <p>Skills</p>
       <span>Here are some of the technologies I am most proficient with: </span>
       <ul className="list-group list-group-horizontal-md d-flex flex-wrap justify-content-around mt-4">
-        <li className="list-group-flush"><img src={html} alt="html logo"/>HTML5</li>
-        <li className="list-group-flush"><img src={css} alt="css logo"/>CSS3</li>
-        <li className="list-group-flush"><img src={csharp} alt="css logo"/>C#</li>
+        <li className="list-group-flush"><img src={csharp} alt="css logo"/></li>
+        <li className="list-group-flush"><img src={core} alt="css logo"/></li>
         <li className="list-group-flush"><img src={js} alt="JavaScript logo"/>JavaScript</li>
         <li className="list-group-flush"><img src={react} alt="react logo"/>React</li>
-      </ul>
-      <span>And some I'm familiar with: </span>
-      <ul className="list-group list-group-horizontal-md d-flex flex-wrap justify-content-around mt-4">
-        <li className="list-group-flush"> <img src={redux} alt="Redux logo"/> Redux</li>
-        <li className="list-group-flush"><img src={php} alt="php logo"/>PHP</li>
-        <li className="list-group-flush"><img src={laravel} alt="sass logo"/>Laravel</li>
       </ul>
     </section>
   )
